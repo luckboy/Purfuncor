08dd4089e47f9b831357bebdb075525c0fa4a2ee
Lukasz Szpakowski <luckboy@vp.pl>
2013-05-29 23:53:25 +0200

    Fixed lexer.

--- src/main/scala/pl/luckboy/purfuncor/frontend/parser/Lexer.scala
+++ src/main/scala/pl/luckboy/purfuncor/frontend/parser/Lexer.scala
@@ -6,8 +6,8 @@ import scalaz.Scalaz._
 
 case class Lexer() extends StdLexical
 {
-  delimiters ++= List("(", ")", "{", "}", "#", ".", "_", "=", "\\", "=>", ";", "\n")
-  reserved ++= List("false", "true", "tuple", "let", "in", "module", "import")
+  delimiters ++= List("(", ")", "{", "}", "#", ".", "=", "\\", "=>", ";", "\n")
+  reserved ++= List("_", "false", "true", "tuple", "let", "in", "module", "import")
   
   case class CharLit(chars: String) extends Token
   case class ByteLit(chars: String) extends Token
