88ac7ea000976c6af475344d652c03329b426345
Lukasz Szpakowski <luckboy@vp.pl>
2013-06-21 17:05:11 +0200

    Added test for interpreter.

--- src/test/scala/pl/luckboy/purfuncor/backend/interp/spec/InterpreterSpec.scala
+++ src/test/scala/pl/luckboy/purfuncor/backend/interp/spec/InterpreterSpec.scala
@@ -2,6 +2,7 @@ package pl.luckboy.purfuncor.backend.interp.spec
 import scalaz._
 import scalaz.Scalaz._
 import org.scalatest.FlatSpec
+import org.scalatest.Inside
 import org.scalatest.matchers.ShouldMatchers
 import pl.luckboy.purfuncor.common._
 import pl.luckboy.purfuncor.frontend.Combinator
@@ -21,6 +22,18 @@ class InterpreterSpec extends FlatSpec with ShouldMatchers
     it should "interpret the term string" in {
       val (env, res) = Interpreter.interpretTermString("#iAdd 2 (#iMul 3 4)")(g).run(emptyEnv)
       res should be ===(IntValue(14).success)
+    }
+    
+    it should "interpret the tree string" in {
+      val (env, res) = Interpreter.interpretTreeString("""
+f = #iAdd g h
+g = #iMul h 2
+h = #iSub 7 4
+""")(f).run(emptyEnv)
+      res should be ===(().success.success)
+      enval.globalVarValueFromEnvironment(env)(GlobalSymbol(NonEmptyList("f"))) should be ===(IntValue(9))
+      enval.globalVarValueFromEnvironment(env)(GlobalSymbol(NonEmptyList("g"))) should be ===(IntValue(6))
+      enval.globalVarValueFromEnvironment(env)(GlobalSymbol(NonEmptyList("h"))) should be ===(IntValue(3))
     }
   }
   
