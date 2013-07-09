9da1253b36c568f60646318762fa55cc5d562f14
Lukasz Szpakowski <luckboy@vp.pl>
2013-07-09 23:02:58 +0200

    Added intKindTermFromKindTerm.

--- src/main/scala/pl/luckboy/purfuncor/frontend/KindTermUtils.scala
+++ src/main/scala/pl/luckboy/purfuncor/frontend/KindTermUtils.scala
@@ -1,4 +1,6 @@
 package pl.luckboy.purfuncor.frontend
+import scalaz._
+import scalaz.Scalaz._
 import pl.luckboy.purfuncor.common._
 import pl.luckboy.purfuncor.common.Arrow
 
@@ -10,4 +12,21 @@ object KindTermUtils
       case Star(KindParam(param), _) => Set(param)
       case _                         => Set()
     }
+  
+  private def intKindTermFromKindTermForParamsS[T](kindTerm: KindTerm[StarKindTerm[T]])(params: Map[T, Int]): (Map[T, Int], KindTerm[StarKindTerm[Int]]) =
+    kindTerm match {
+      case Arrow(arg, ret, pos) =>
+        val (params2, arg2) = intKindTermFromKindTermForParamsS(arg)(params)
+        val (params3, ret2) = intKindTermFromKindTermForParamsS(ret)(params2)
+        (params3, Arrow(arg2, ret2, pos))
+      case Star(KindType, pos) =>
+        (params, Star(KindType, pos))
+      case Star(KindParam(param), pos) =>
+        params.get(param).map {
+          param2 => (params, Star(KindParam(param2), pos))
+        }.getOrElse((params + (param -> params.size), Star(KindParam(params.size), pos)))
+    }
+  
+  def intKindTermFromKindTerm[T](kindTerm: KindTerm[StarKindTerm[T]]) =
+    intKindTermFromKindTermForParamsS(kindTerm)(Map())._2
 }
