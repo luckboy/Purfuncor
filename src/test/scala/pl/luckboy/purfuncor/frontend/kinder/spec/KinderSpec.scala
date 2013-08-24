43dc91aa37c4cc59f6331a30a7f64274467dc9a5
Lukasz Szpakowski <luckboy@vp.pl>
2013-08-24 23:44:58 +0200

    Added second test for kinder.

--- src/test/scala/pl/luckboy/purfuncor/frontend/kinder/spec/KinderSpec.scala
+++ src/test/scala/pl/luckboy/purfuncor/frontend/kinder/spec/KinderSpec.scala
@@ -27,6 +27,7 @@ type U t u = t u
       // T
       inside(enval.globalTypeVarKindFromEnvironment(env)(GlobalSymbol(NonEmptyList("T")))) {
         case InferredKind(Arrow(arg1, ret1, _)) =>
+          // k1 -> k1
           inside(arg1) {
             case Star(KindParam(param1), _) =>
               inside(ret1) {
@@ -45,6 +46,7 @@ type U t u = t u
       // U
       inside(enval.globalTypeVarKindFromEnvironment(env)(GlobalSymbol(NonEmptyList("U")))) {
         case InferredKind(Arrow(arg1, Arrow(arg2, ret2, _), _)) =>
+          // (k1 -> k2) -> k1 -> k2
           inside(arg1) {
             case Arrow(arg11, ret11, _) =>
               inside(arg11) {
@@ -69,6 +71,7 @@ type U t u = t u
           kinds should have size(2)
           inside(kinds.get(LocalSymbol("t"))) {
             case Some(InferredKind(Arrow(arg1, ret1, _))) =>
+              // k1 -> k2
               inside(arg1) {
                 case Star(KindParam(param1), _) =>
                   inside(ret1) {
@@ -79,6 +82,61 @@ type U t u = t u
           }
           inside(kinds.get(LocalSymbol("u"))) {
             case Some(InferredKind(Star(KindParam(_), _))) => ()
+          }
+      }
+    }
+
+    it should "infer the kinds from the string with the lambda-expression" in {
+      val (env, res) = Kinder.inferKindsFromTreeString("type T t = (\\u v => v u) t")(NameTree.empty)(f).run(emptyEnv)
+      res should be ===(().success.success)
+      inside(enval.globalTypeVarKindFromEnvironment(env)(GlobalSymbol(NonEmptyList("T")))) {
+        case InferredKind(Arrow(arg1, ret1, _)) =>
+          // k1 -> (k1 -> k2) -> k2
+          inside(arg1) {
+            case Star(KindParam(param1), _) =>
+              inside(ret1) {
+                case Arrow(arg2, ret2, _) =>
+                  inside(arg2) {
+                    case Arrow(arg21, ret21, _) =>
+                      inside(arg21) {
+                        case Star(KindParam(param21), _) =>
+                          inside(ret21) {
+                            case Star(KindParam(param22), _) =>
+                              inside(ret2) {
+                                case Star(KindParam(param3), _) =>
+                                  List(param1, param21).toSet should have size(1)
+                                  List(param22, param3).toSet should have size(1)
+                                  List(param1, param21, param22, param3).toSet should have size(2)
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+      inside(enval.localKindTablesFromEnvironment(env)(some(GlobalSymbol(NonEmptyList("T")))).get(0)) {
+        case Some(KindTable(kinds)) =>
+          kinds should have size(1)
+          inside(kinds.get(LocalSymbol("t"))) {
+            case Some(InferredKind(Star(KindParam(_), _))) => ()
+          }          
+      }
+      inside(enval.localKindTablesFromEnvironment(env)(some(GlobalSymbol(NonEmptyList("T")))).get(1)) {
+        case Some(KindTable(kinds)) =>
+          kinds should have size(2)
+          inside(kinds.get(LocalSymbol("u"))) {
+            case Some(InferredKind(Star(KindParam(_), _))) => ()
+          }        
+          inside(kinds.get(LocalSymbol("v"))) {
+            case Some(InferredKind(Arrow(arg1, ret1, _))) =>
+              // k1 -> k2
+              inside(arg1) {
+                case Star(KindParam(param1), _) =>
+                  inside(ret1) {
+                    case Star(KindParam(param2), _) =>
+                      List(param1, param2).toSet should have size(2)
+                  }
+              }
           }
       }
     }
