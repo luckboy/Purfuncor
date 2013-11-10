b652e49663269b63d85f251ea73403203d333946
Lukasz Szpakowski <luckboy@vp.pl>
2013-11-10 17:34:20 +0100

    Implemented three tests.

--- src/test/scala/pl/luckboy/purfuncor/frontend/typer/spec/TyperSpec.scala
+++ src/test/scala/pl/luckboy/purfuncor/frontend/typer/spec/TyperSpec.scala
@@ -128,11 +128,231 @@ h x y = x y
       }
     }
     
-    it should "infer the type from the string with the let-expression" is (pending)
+    it should "infer the type from the string with the let-expression" in {
+      val (env, res) = Typer.inferTypesFromTreeString("""
+f g x = let
+    a = g x
+    b = tuple 2 x true
+  in
+    tuple 2 a b
+""")(NameTree.empty)(f).run(emptyEnv)
+      res should be ===(().success.success)
+      inside(enval.globalVarTypeFromEnvironment(env)(GlobalSymbol(NonEmptyList("f")))) {
+        case InferredType(BuiltinType(TypeBuiltinFunction.Fun, Seq(argType1, retType1)), argKinds) =>
+          // \t1 t2 => (t1 #-> t2) #-> t1 #-> (t2, (t1, #Boolean))
+          inside(argType1) {
+            case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType11, retType11)) =>
+              inside(argType11) {
+                case TypeParamApp(param11, Seq(), 0) =>
+                  inside(retType11) {
+                    case TypeParamApp(param12, Seq(), 0) =>
+                      inside(retType1) {
+                        case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType2, retType2)) =>
+                          inside(argType2) {
+                            case TypeParamApp(param2, Seq(), 0) =>
+                              inside(retType2) {
+                                case TupleType(Seq(type21, type22)) =>
+                                  inside(type21) {
+                                    case TypeParamApp(param21, Seq(), 0) =>
+                                      inside(type22) {
+                                        case TupleType(Seq(TypeParamApp(param22, Seq(), 0), BuiltinType(TypeBuiltinFunction.Boolean, Seq()))) =>
+                                          List(param11, param2, param22).toSet should have size(1)
+                                          List(param12, param21).toSet should have size(1)
+                                          List(param11, param12, param2, param21, param22).toSet should have size(2)
+                                      }
+                                  }
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+          inside(argKinds) {
+            case Seq(
+                InferredKind(Star(KindType, _)) /* * */,
+                InferredKind(Star(KindType, _)) /* * */) =>
+              ()
+          }
+      }
+      inside(enval.lambdaInfosFromEnvironment(env)(Some(GlobalSymbol(NonEmptyList("f")))).get(0)) {
+        case Some(InferenceLambdaInfo(TypeTable(types), Seq())) =>
+          types should have size(2)
+          inside(types.get(LocalSymbol("g"))) {
+            case Some(InferredType(BuiltinType(TypeBuiltinFunction.Fun, Seq(argType1, retType1)), argKinds)) =>
+              inside(argType1) {
+                case TypeParamApp(param1, Seq(), 0) =>
+                  inside(retType1) {
+                    case TypeParamApp(param2, Seq(), 0) =>
+                      List(param1, param2).toSet should have size(2)
+                  }
+              }
+              inside(argKinds) {
+                case Seq(InferredKind(Star(KindType, _)), InferredKind(Star(KindType, _))) =>
+                  ()
+              }
+          }
+          inside(types.get(LocalSymbol("x"))) {
+            case Some(InferredType(TypeParamApp(_, Seq(), 0), Seq(InferredKind(Star(KindType, _))))) =>
+              ()
+          }
+      }
+      inside(enval.lambdaInfosFromEnvironment(env)(Some(GlobalSymbol(NonEmptyList("f")))).get(1)) {
+        case Some(InferenceLambdaInfo(TypeTable(types), Seq())) =>
+          types should have size(2)
+          inside(types.get(LocalSymbol("a"))) {
+            case Some(InferredType(TypeParamApp(_, Seq(), 0), Seq(InferredKind(Star(KindType, _))))) =>
+              ()
+          }
+          inside(types.get(LocalSymbol("b"))) {
+            case Some(InferredType(TupleType(Seq(TypeParamApp(_, Seq(), 0), BuiltinType(TypeBuiltinFunction.Boolean, Seq()))), Seq(InferredKind(Star(KindType, _))))) =>
+              ()
+          }
+      }
+    }
     
-    it should "infer the type from the string with the lambda-expression" is (pending)
+    it should "infer the type from the string with the lambda-expression" in {
+      val (env, res) = Typer.inferTypesFromTreeString("""
+f g x = (\y h => tuple 2 (g x) (h y)) x
+""")(NameTree.empty)(f).run(emptyEnv)
+      res should be ===(().success.success)
+      inside(enval.globalVarTypeFromEnvironment(env)(GlobalSymbol(NonEmptyList("f")))) {
+        case InferredType(BuiltinType(TypeBuiltinFunction.Fun, Seq(argType1, retType1)), argKinds) =>
+          // \t1 t2 t3 => (t1 #-> t2) #-> t1 #-> (t1 #-> t3) #-> (t2, t3)
+          inside(argType1) {
+            case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType11, retType11)) =>
+              inside(argType11) {
+                case TypeParamApp(param11, Seq(), 0) =>
+                  inside(retType11) {
+                    case TypeParamApp(param12, Seq(), 0) =>
+                      inside(retType1) {
+                        case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType2, retType2)) =>
+                          inside(argType2) {
+                            case TypeParamApp(param2, Seq(), 0) =>
+                              inside(retType2) {
+                                case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType3, retType3)) =>
+                                  inside(argType3) {
+                                    case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType31, retType31)) =>
+                                      inside(argType31) {
+                                        case TypeParamApp(param31, Seq(), 0) =>
+                                          inside(retType31) {
+                                            case TypeParamApp(param32, Seq(), 0) =>
+                                              inside(retType3) {
+                                                case TupleType(Seq(TypeParamApp(param33, Seq(), 0), TypeParamApp(param34, Seq(), 0))) =>
+                                                  List(param11, param2, param31).toSet should have size (1)
+                                                  List(param12, param33).toSet should have size(1)
+                                                  List(param32, param34).toSet should have size(1)
+                                                  List(param11, param12, param2, param31, param32, param33, param34).toSet should have size(3)
+                                              }
+                                          }
+                                      }
+                                  }
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+          inside(argKinds) {
+            case Seq(
+                InferredKind(Star(KindType, _)) /* * */,
+                InferredKind(Star(KindType, _)) /* * */,
+                InferredKind(Star(KindType, _)) /* * */) =>
+              ()
+          }
+      }
+      inside(enval.lambdaInfosFromEnvironment(env)(Some(GlobalSymbol(NonEmptyList("f")))).get(0)) {
+        case Some(InferenceLambdaInfo(TypeTable(types), Seq())) =>
+          types should have size(2)
+          inside(types.get(LocalSymbol("g"))) {
+            case Some(InferredType(BuiltinType(TypeBuiltinFunction.Fun, Seq(argType1, retType1)), argKinds)) =>
+              inside(argType1) {
+                case TypeParamApp(param1, Seq(), 0) =>
+                  inside(retType1) {
+                    case TypeParamApp(param2, Seq(), 0) =>
+                      List(param1, param2).toSet should have size(2)
+                  }
+              }
+              inside(argKinds) {
+                case Seq(InferredKind(Star(KindType, _)), InferredKind(Star(KindType, _))) =>
+                  ()
+              }
+          }
+          inside(types.get(LocalSymbol("x"))) {
+            case Some(InferredType(TypeParamApp(_, Seq(), 0), Seq(InferredKind(Star(KindType, _))))) =>
+              ()
+          }
+      }
+      inside(enval.lambdaInfosFromEnvironment(env)(Some(GlobalSymbol(NonEmptyList("f")))).get(1)) {
+        case Some(InferenceLambdaInfo(TypeTable(types), Seq())) =>
+          types should have size(2)
+          inside(types.get(LocalSymbol("y"))) {
+            case Some(InferredType(TypeParamApp(_, Seq(), 0), Seq(InferredKind(Star(KindType, _))))) =>
+              ()
+          }
+          inside(types.get(LocalSymbol("h"))) {
+            case Some(InferredType(BuiltinType(TypeBuiltinFunction.Fun, Seq(argType1, retType1)), argKinds)) =>
+              inside(argType1) {
+                case TypeParamApp(param1, Seq(), 0) =>
+                  inside(retType1) {
+                    case TypeParamApp(param2, Seq(), 0) =>
+                      List(param1, param2).toSet should have size(2)
+                  }
+              }
+              inside(argKinds) {
+                case Seq(InferredKind(Star(KindType, _)), InferredKind(Star(KindType, _))) =>
+                  ()
+              }
+          }
+      }
+    }
     
-    it should "infer the type from the string with the nested lambda-expression" is (pending)
+    it should "infer the type from the string with the nested lambda-expression" in {
+      val (env, res) = Typer.inferTypesFromTreeString("""
+f x1 = \x2 g => \h x3 => #fAdd (#fMul x1 x3) (g (h x2))
+""")(NameTree.empty)(f).run(emptyEnv)
+      res should be ===(().success.success)
+      inside(enval.globalVarTypeFromEnvironment(env)(GlobalSymbol(NonEmptyList("f")))) {
+        case InferredType(BuiltinType(TypeBuiltinFunction.Fun, Seq(argType1, retType1)), argKinds) =>
+          // \t1 t2 => #Float #-> t1 #-> (t2 #-> #Float) #-> (t1 #-> t2) #-> #Float #-> #Float
+          inside(argType1) { case BuiltinType(TypeBuiltinFunction.Float, Seq()) => () }
+          inside(retType1) {
+            case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType2, retType2)) =>
+              inside(argType2) {
+                case TypeParamApp(param2, Seq(), 0) =>
+                  inside(retType2) {
+                    case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType3, retType3)) =>
+                      inside(argType3) {
+                        case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType31, retType31)) =>
+                          inside(argType31) {
+                            case TypeParamApp(param31, Seq(), 0) =>
+                              inside(retType31) { case BuiltinType(TypeBuiltinFunction.Float, Seq()) => () }
+                              inside(retType3) {
+                                case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType4, retType4)) =>
+                                  inside(argType4) {
+                                    case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType41, retType41)) =>
+                                      inside(argType41) {
+                                        case TypeParamApp(param41, Seq(), 0) =>
+                                          inside(retType41) {
+                                            case TypeParamApp(param42, Seq(), 0) =>
+                                              inside(retType4) {
+                                                case BuiltinType(TypeBuiltinFunction.Fun, Seq(argType5, retType5)) =>
+                                                  inside(argType5) { case BuiltinType(TypeBuiltinFunction.Float, Seq()) => () }
+                                                  inside(retType5) { case BuiltinType(TypeBuiltinFunction.Float, Seq()) => () }
+                                                  List(param2, param41).toSet should have size(1)
+                                                  List(param31, param42).toSet should have size(1)
+                                                  List(param2, param31, param41, param42).toSet should have size(2)
+                                              }
+                                          }
+                                      }
+                                  }
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+    }
 
     it should "infer the type from the covered local variables" is (pending)
     
