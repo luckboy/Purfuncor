879ccabd85ed39befa7fae0586f28e02f1b31fb6
Lukasz Szpakowski <luckboy@vp.pl>
2013-11-24 17:12:33 +0100

    Added next test without implementation.

--- src/test/scala/pl/luckboy/purfuncor/frontend/typer/spec/TyperSpec.scala
+++ src/test/scala/pl/luckboy/purfuncor/frontend/typer/spec/TyperSpec.scala
@@ -3447,7 +3447,9 @@ h = tuple 2 (construct 0: T) (construct 0: U)
     }
     
     it should "transform the string of the term with the type inference" is (pending)
-    
+
+    it should "transform the string of the term with the type inference and global variables" is (pending)
+
     it should "complain on transformation of the incorrect string" is (pending)
   }
   
