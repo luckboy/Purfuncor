fed7a7fc583909546c28490ceb3938537d12dd09
Lukasz Szpakowski <luckboy@vp.pl>
2013-12-04 19:24:00 +0100

    Added InstanceTable without implemented methods and GlobalInstanceTable.

---
+++ src/main/scala/pl/luckboy/purfuncor/frontend/typer/InstanceTable.scala
@@ -0,0 +1,21 @@
+package pl.luckboy.purfuncor.frontend.typer
+import scalaz._
+import scalaz.Scalaz._
+import pl.luckboy.purfuncor.common._
+import pl.luckboy.purfuncor.frontend._
+
+case class InstanceTable[T, U](pairs: Seq[(Type[T], U)])
+{
+  def findTypesS[V, E](typ: Type[T])(env: E)(implicit unifier: Unifier[NoType[T], TypeValueTerm[T], E, Int], envSt: TypeInferenceEnvironmentState[E, V, T]): (E, Validation[NoType[T], Seq[Type[T]]]) =
+    throw new UnsupportedOperationException
+  
+  def addTypeS[V, E](typ: Type[T])(env: E)(implicit unifier: Unifier[NoType[T], TypeValueTerm[T], E, Int], envSt: TypeInferenceEnvironmentState[E, V, T]): (E, Validation[NoType[T], Option[InstanceTable[T, U]]]) =
+    throw new UnsupportedOperationException
+}
+
+object InstanceTable
+{
+  def empty[T, U] = InstanceTable[T, U](Seq())
+  
+  def fromGlobalInstanceTable[T, U](instTable: GlobalInstanceTable[T, U]) = InstanceTable[T, U](instTable.pairs)
+}
