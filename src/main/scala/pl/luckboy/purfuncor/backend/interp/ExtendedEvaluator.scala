91a4409bc4f156b5e16bb663b86c074f3f4309cc
Lukasz Szpakowski <luckboy@vp.pl>
2014-01-03 19:52:46 +0100

    Refactoring.

--- src/main/scala/pl/luckboy/purfuncor/backend/interp/ExtendedEvaluator.scala
+++ src/main/scala/pl/luckboy/purfuncor/backend/interp/ExtendedEvaluator.scala
@@ -4,11 +4,11 @@ import scalaz.Scalaz._
 import pl.luckboy.purfuncor.common._
 import pl.luckboy.purfuncor.frontend._
 
-trait ExtendedEvaluator[L, E, V]
+trait ExtendedEvaluator[I, E, V]
 {
-  def variableS(value: V, lambdaInfo: L)(env: E): (E, V)
+  def variableS(value: V, lambdaInfo: I)(env: E): (E, V)
   
-  def constructS(n: Int, lambdaInfo: L)(env: E): (E, V)
+  def constructS(n: Int, lambdaInfo: I)(env: E): (E, V)
   
-  def selectS[T, U](value: V, cases: Seq[Case[T, L, U]], lambdaInfo: L)(env: E): (E, Validation[V, Case[T, L, U]])
+  def selectS[T, U](value: V, cases: Seq[Case[T, I, U]], lambdaInfo: I)(env: E): (E, Validation[V, Case[T, I, U]])
 }
