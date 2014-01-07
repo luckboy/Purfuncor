5ce9f353bfd88dbc273234c435652a42eaf06f50
Lukasz Szpakowski <luckboy@vp.pl>
2014-01-07 22:57:13 +0100

    Added license.

--- src/main/scala/pl/luckboy/purfuncor/frontend/instant/Instance.scala
+++ src/main/scala/pl/luckboy/purfuncor/frontend/instant/Instance.scala
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Łukasz Szpakowski.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ ******************************************************************************/
 package pl.luckboy.purfuncor.frontend.instant
 import scala.util.parsing.input.Position
 import scalaz._
