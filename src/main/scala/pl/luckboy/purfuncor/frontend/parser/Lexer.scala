ab6355ea9a5dad5fbfa473a650a560aa5f2a2f46
Lukasz Szpakowski <luckboy@vp.pl>
2014-01-20 18:08:01 +0100

    Fixed.

--- src/main/scala/pl/luckboy/purfuncor/frontend/parser/Lexer.scala
+++ src/main/scala/pl/luckboy/purfuncor/frontend/parser/Lexer.scala
@@ -13,9 +13,9 @@ import scalaz.Scalaz._
 
 case class Lexer() extends StdLexical
 {
-  delimiters ++= List("(", ")", "{", "}", "[", "]", "#", "##", ".", "\\", ";", "\n", "###")
+  delimiters ++= List("(", ")", "{", "}", "[", "]", "#", "##", ".", ";", "\n", "###")
   reserved ++= List("_", "false", "true", "tuple", "let", "in", "module", "import", "type", "unittype", "construct", 
-      "select", "extract", "poly", "instance", "makearray", "makelist", "fieldset", "=", "=>", ":", "*", "!")
+      "select", "extract", "poly", "instance", "makearray", "makelist", "fieldset", "=", "\\", "=>", ":", "*", "!")
   
   case class CharLit(chars: String) extends Token
   case class ByteLit(chars: String) extends Token
